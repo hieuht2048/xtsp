1144aa1ff79ca46d19388a9ec0ce183838f6604e
hieuht2048 <hieu.ht2048@gmail.com>
2019-04-11 01:32:29 +0700

    Updates

---
+++ static/js/20.f6e913ec.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{262:function(n,t,e){"use strict";e.r(t);var a=e(18),c=e(19),u=e(21),o=e(20),r=e(22),i=e(0),p=e.n(i),s=function(n){function t(){return Object(a.a)(this,t),Object(u.a)(this,Object(o.a)(t).apply(this,arguments))}return Object(r.a)(t,n),Object(c.a)(t,[{key:"render",value:function(){return p.a.createElement("div",null,"Not found!")}}]),t}(i.Component);t.default=s}}]);
+//# sourceMappingURL=20.f6e913ec.chunk.js.map
